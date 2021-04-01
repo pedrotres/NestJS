66bd2a7b4f1e347c0bd65f4f2cb1be5507c80e21
pedrotres <52545592+pedrotres@users.noreply.github.com>
2021-03-31 22:22:56 -0300

    validações na entidade categorias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JogadoresModule } from './jogadores/jogadores.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CategoriasModule } from './categorias/categorias.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
@@ -13,6 +14,7 @@ import { MongooseModule } from '@nestjs/mongoose';
       },
     ),
     JogadoresModule,
+    CategoriasModule,
   ],
   controllers: [],
   providers: [],
